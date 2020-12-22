040ad8ae2d6a988b29b14eb6eca957cee8ce74fa
Splatterxl <splatterxl@outlook.ie>
2020-12-22 22:01:09 +0000

    animations

--- base/assets/main.js
+++ base/assets/main.js
@@ -4,3 +4,24 @@ document.querySelector('settings js-link#theme-toggle').addEventListener('click'
     if (localStorage.getItem('theme') == null || localStorage.getItem('theme') == 'light') localStorage.setItem('theme', 'dark'); else if (localStorage.getItem('theme') == 'dark') localStorage.setItem('theme', 'light');
     location.reload();
 });
+
+setInterval(() =>
+{
+    let element = document.getElementById('creating');
+    let anims = {
+        'D': 'Di',
+        'Di': 'Dis',
+        'Dis': 'Disc',
+        'Disc': 'Disco',
+        'Disco': 'Discor',
+        'Discor': 'Discord',
+        'Discord': 'Discord B',
+        'Discord B': 'Discord Bo',
+        'Discord Bo': 'Discord Bots',
+        'Discord Bots': 'Discord Bots_',
+        'Discord Bots_': 'Discord Bots'
+    };
+
+    element.textContent = anims[element.textContent];
+
+}, 500);
