d77306f29fc8057c2e4a9d8fcc017d413895fd6d
Splatterxl <splatterxl@outlook.ie>
2020-12-23 19:36:41 +0000

    added fonts and other things

--- base/assets/js/main.js
+++ base/assets/js/main.js
@@ -1,8 +1,8 @@
 document.querySelector('fieldset nav js-link#projects-drop-down-button').addEventListener('click', () =>
 {
     document.getElementById('projects-drop-down').innerHTML =
-        !(document.getElementById('projects-drop-down').innerHTML == `<a href=\"http://github.com/Splatterxl/rcorp_files\" class=\"blurple\">see my latest failure</a>\n|\n<a href=\"http://splatterxl.github.io\" class=\"blurple\">this thing</a>\n|\n<a href=\"utilitybot\" class=\"blurple\">my discord bot</a>`)
-            ? `<a href="http://github.com/Splatterxl/rcorp_files"\nclass='blurple'>see my latest failure</a>\n|\n<a href="http://splatterxl.github.io"\nclass='blurple'>this thing</a>\n|\n<a href="utilitybot"\nclass='blurple'>my discord bot</a>`
+        !(document.getElementById('projects-drop-down').innerHTML == `<a href=\"http://splatterxl.github.io\" class=\"blurple\">This Website</a>\n|\n<a href=\"utilitybot\" class=\"blurple\">Eureka! - My Discord Bot</a>`)
+            ? `<a href="http://splatterxl.github.io"\nclass='blurple'>This Website</a>\n|\n<a href="utilitybot"\nclass='blurple'>Eureka! - My Discord Bot</a>`
             : ' ';
 });
 document.querySelector('settings js-link#theme-toggle').addEventListener('click', () =>
@@ -13,15 +13,6 @@ document.querySelector('settings js-link#theme-toggle').addEventListener('click'
 
 document.getElementById('creating').textContent = (Math.round(Math.random()) === 1) ? 'D' : (Math.round(Math.random()) === 1) ? 'W' : 'S';
 
-// Hi yuto > Hi everyone
-// Well, he's currently away < alright
-// I remember C# code format
-/*
-if (...)
-{
-    code
-}
-*/
 const elem = document.getElementById('creating');
 const strs = ['Discord Bots', 'Websites', 'stuff'];
 const base = (elem.textContent == 'D') ? 'Discord Bots' : (elem.textContent == 'W') ? 'Websites' : 'stuff';
