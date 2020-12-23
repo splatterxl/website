3104d6e26b6cdca47157970dda62c9e3697bcbd7
Splatterxl <splatterxl@outlook.ie>
2020-12-23 18:23:42 +0000

    optimised the animations

--- base/assets/main.js
+++ base/assets/main.js
@@ -5,23 +5,41 @@ document.querySelector('settings js-link#theme-toggle').addEventListener('click'
     location.reload();
 });
 
+document.getElementById('creating').textContent = (Math.round(Math.random()) === 1) ? 'D' : (Math.round(Math.random()) === 1) ? 'W' : 'S';
+
+// Hi yuto > Hi everyone
+// Well, he's currently away < alright
+// I remember C# code format
+/*
+if (...)
+{
+    code
+}
+*/
+const elem = document.getElementById('creating');
+const strs = ['Discord Bots', 'Websites', 'stuff'];
+const base = (elem.textContent == 'D') ? 'Discord Bots' : (elem.textContent == 'W') ? 'Websites' : 'stuff';
+elem.textContent = '';
+
+
+
+// I have to push, wait a sec
+// https://splatterxl.tk
+// Alright, I just forgot it lmao
+
+const anim = () =>
+{
+    if (strs.includes(elem.textContent.endsWith('_') ? elem.textContent.slice(0, elem.textContent.length - 1) : elem.textContent))
+    {
+        if (elem.textContent.endsWith('_'))
+            return elem.textContent = elem.textContent.slice(0, elem.textContent.length - 1);
+        else
+            return elem.textContent += '_';
+    }
+    elem.textContent += base[elem.textContent.length] ? base[elem.textContent.length] : '';
+};
+
 setInterval(() =>
 {
-    let element = document.getElementById('creating');
-    let anims = {
-        'D': 'Di',
-        'Di': 'Dis',
-        'Dis': 'Disc',
-        'Disc': 'Disco',
-        'Disco': 'Discor',
-        'Discor': 'Discord',
-        'Discord': 'Discord B',
-        'Discord B': 'Discord Bo',
-        'Discord Bo': 'Discord Bots',
-        'Discord Bots': 'Discord Bots_',
-        'Discord Bots_': 'Discord Bots'
-    };
-
-    element.textContent = anims[element.textContent];
-
+    anim();
 }, 500);
