515ea06b494b121bda28977c68c46b99107ddeeb
Splatterxl <splatterxl@outlook.ie>
2022-11-17 18:36:00 +0000

    add steam link
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
-import { FaGithub, FaScroll, FaTwitter } from 'react-icons/fa';
-import { IoMdContact } from 'react-icons/io';
+import { FaGithub, FaScroll, FaSteam, FaTwitter } from 'react-icons/fa';
 
 const links = [
   { icon: FaGithub, url: 'https://github.com/splatterxl', name: 'GitHub' },
   { icon: FaTwitter, url: 'https://twitter.com/wontfixbug', name: 'Twitter' },
+  {
+    icon: FaSteam,
+    url: 'https://steamcommunity.com/id/ruair1/',
+    name: 'Steam',
+  },
   { icon: FaScroll, url: '/blog', name: 'Blog' },
   // { icon: CgProfile, url: '/about', name: 'About' },
 ];
@@ -14,7 +18,11 @@ export const Icons: React.FC = () => {
     <div className="flex flex-row justify-center md:justify-start items-center mt-3 ml-2 gap-1 w-full">
       {links.map((link) => (
         <Link key={link.url} href={link.url} passHref>
-          <a aria-labelledby={link.name + '__label'}>
+          <a
+            aria-labelledby={link.name + '__label'}
+            target={link.url.startsWith('http') ? '_blank' : '_self'}
+            rel="noopener"
+          >
             <link.icon size="1.5rem" />
             <span
               style={{ display: 'none' }}
