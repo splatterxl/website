d3adb8ee90efc5ab645a5e6d1dd6f1afb67bd20c
Splatterxl <splatterxl@outlook.ie>
2023-01-26 18:27:39 +0000

    nop
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
-import '../util/console';
+import '../utils/console';
 
 export default function Portfolio({ Component, pageProps }: any) {
   return (
