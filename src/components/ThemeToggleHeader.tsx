27b17c01f71ee2d3d84499cd1a39dc21c54b5e16
Splatterxl <splatterxl@outlook.ie>
2023-04-07 00:20:49 +0100

    theme toggle header is now confetti button
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/components/ThemeToggleHeader.tsx
+++ src/components/ThemeToggleHeader.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from 'next-themes';
+import { confettiBurst } from '../utils/confetti';
 
 export const ThemeToggleHeader: React.FC = () => {
   const { setTheme, resolvedTheme } = useTheme();
@@ -6,21 +7,17 @@ export const ThemeToggleHeader: React.FC = () => {
   return (
     <header
       className="flex flex-row justify-center md:justify-start pb-1 group w-auto md:w-full hover:cursor-pointer"
-      onClick={() =>
-        setTimeout(() => {
-          setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
-        }, 150)
-      }
+      onClick={() => {
+        confettiBurst(100);
+
+        // setTimeout(() => {
+        //   setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+        // }, 150);
+      }}
     >
-      <h1 className="text-[3.6rem] leading-none mb-1 md:leading-none md:text-8xl font-bold select-none text-center md:text-left group-active:translate-y-1">
+      <h1 className="text-[3.6rem] leading-none mb-1 md:leading-none md:text-8xl font-bold select-none text-center md:text-left">
         Splatterxl
       </h1>
-      {/* <span
-        aria-hidden
-        className="justify-end flex-col hidden xs:flex group-active:translate-y-1 select-none italic"
-      >
-        he/him
-      </span> */}
     </header>
   );
 };
