399468724879849b328d9c12dc1ce0c884918b9f
Ruairi <splatterxl@outlook.ie>
2023-07-20 17:29:35 +0100

    Delete assets directory

--- assets/js/global.js
+++
@@ -1,6 +0,0 @@
-// This can be set to 16x16, 36x36, or 72x72
-twemoji.size = '36x36';
-
-// Parse the document body and
-// insert <img> tags in place of Unicode Emojis
-twemoji.parse(document.body);
